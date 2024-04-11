e63f5c91d0eb026646b9a892988e2324996136f5
Raphael Uwadone <raphaeluwadone@gmail.com>
2024-04-11 18:43:08 +0100

    fixes

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,7 +3,7 @@ import Container from "@components/Container";
 import Footer from "@components/Footer";
 import Header from "@components/Header";
 import MainContact from "@components/MainContact";
-import FormfacadeEmbed from "@formfacade/embed-react";
+// import FormfacadeEmbed from "@formfacade/embed-react";
 
 const Contact: React.FC = () => {
     return (
